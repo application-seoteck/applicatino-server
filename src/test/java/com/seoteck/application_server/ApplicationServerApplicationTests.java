0048b471d131911e6d157fc0776e3f59e4224791
kwoo28 <148550522+kwoo28@users.noreply.github.com>
2024-06-16 03:43:55 +0900

    feat: 초기설정

---
+++ src/test/java/com/seoteck/application_server/ApplicationServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.seoteck.application_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
