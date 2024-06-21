241dbcce20d959633117087b9fe513f84a07db35
kwoo28 <148550522+kwoo28@users.noreply.github.com>
2024-06-22 00:39:46 +0900

    feat: health check 추가

---
+++ src/main/java/com/seoteck/application_server/domain/api/controller/HealthController.java
@@ -0,0 +1,13 @@
+package com.seoteck.application_server.domain.api.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HealthController {
+    @GetMapping("/actuator/health")
+    public ResponseEntity showVersion() {
+        return ResponseEntity.ok().body("OK");
+    }
+}
